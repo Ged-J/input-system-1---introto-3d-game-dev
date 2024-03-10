171d4a60bf6cda1e926bba396bc80a2b0cb5ca97
gedvydas <74902029+Ged-J@users.noreply.github.com>
2024-03-10 19:15:57 +0000

    complete

---
+++ Assets/Scripts/TestInputSystem.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TestInputSystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TestJump(InputAction.CallbackContext context)
+    {
+        Debug.Log("Test Jump" + context.phase);
+    }
+
+}
